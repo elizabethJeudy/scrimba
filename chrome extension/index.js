3ae531a3b689b93dd5a926cbbdcf73a9f97b6430
Elizabeth <tjeudy.e@gmail.com>
2023-03-17 19:18:15 -0400

    added delete function

--- chrome extension/index.js
+++ chrome extension/index.js
@@ -1,17 +1,27 @@
 // assign myLeads to empty array
-// assign inputel to input field
+// assign inputEl to input field
 let myLeads = [];
 
 const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const deleteBtn = document.querySelector("#delete-btn");
 const ulEl = document.querySelector("#ul-el");
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
 	myLeads = leadsFromLocalStorage;
 	renderLeads();
 }
 
-document.addEventListener("click", function () {
+// clears saved input from storage, leads, and DOM
+deleteBtn.addEventListener("dblclick", function () {
+	localStorage.clear();
+	myLeads = [];
+	renderLeads();
+});
+
+inputBtn.addEventListener("click", function () {
 	myLeads.push(inputEl.value);
 	inputEl.value = "";
 	// Save the myLeads array to localStorage
